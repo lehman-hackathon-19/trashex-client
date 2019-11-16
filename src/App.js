84bcd2d525f2d7c759d9fe077b784f7e50877a3a
Benjamin Yi <bentaebox@gmail.com>
2019-11-16 10:10:23 -0500

    add a page listing pickups which can be clicked and removed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
+import Pickups from "./pages/Pickups";
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/pickups" component={Pickups}></Route>
         <Route path="/dashboard" component={Dashboard}></Route>
         <Route path="/" component={LandingPage}></Route>
       </Switch>

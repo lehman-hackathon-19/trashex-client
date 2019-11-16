a4d410bd29b4b6e029a964291fe0216e87d1f30c
Aswin Nair <aswintorch@gmail.com>
2019-11-15 22:16:45 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [msg, setMsg] = useState("")
+
+  useEffect(() => {
+    fetchData();
+  })
+
+  const fetchData = async () => {
+    fetch('https://cogent-altar-259201.appspot.com/')
+      .then(response => console.log(response))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{msg}</h1>
     </div>
   );
 }

1bb73437b24b52bcfc018e9c8d1999eb0db754c1
Aswin Nair <aswintorch@gmail.com>
2019-11-15 23:20:06 -0500

    Added dashboard page and removed api call in landing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
-import './App.css';
-import LandingPage from './pages/LandingPage'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import "./App.css";
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  
-
   return (
-    <Router >
+    <Router>
       <Switch>
-        <Route to="/" component={LandingPage}></Route>
+        <Route path="/dashboard" component={Dashboard}></Route>
+        <Route path="/" component={LandingPage}></Route>
       </Switch>
-      
     </Router>
   );
 }
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from "react";
 
 const LandingPage = () => {
-  const [msg, setMsg] = useState("");
+  // const [msg, setMsg] = useState("");
 
-  useEffect(() => {
-    fetchData();
-  });
+  // useEffect(() => {
+  //   fetchData();
+  // });
 
-  const fetchData = async () => {
-    fetch("https://trashex.herokuapp.com/")
-      .then(res => res.text())
-      .then(res => setMsg(res))
-  };
+  // const fetchData = async () => {
+  //   fetch("https://trashex.herokuapp.com/")
+  //     .then(res => res.text())
+  //     .then(res => setMsg(res))
+  // };
 
   return (
     <div className="container text-center">
-      <h1>{msg}</h1>
+      <h1>Welcome to The Trash Exchange</h1>
     </div>
   );
 };

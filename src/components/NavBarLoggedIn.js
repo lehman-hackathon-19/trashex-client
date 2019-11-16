e31e762890855d3869345122f1116ec8a7d38ce5
Aswin Nair <aswintorch@gmail.com>
2019-11-16 05:35:22 -0500

    A

--- src/components/NavBarLoggedIn.js
+++ src/components/NavBarLoggedIn.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import axios from 'axios'
+import axios from "axios";
 
 const NavbarLoggedIn = () => {
   const [userFullName, setUserFullName] = useState("");
@@ -10,8 +10,8 @@ const NavbarLoggedIn = () => {
   });
 
   const fetchUser = async () => {
-    const res = await axios.get('/api/current_user')
-    setUserFullName(res.data.name)
+    const res = await axios.get("/api/current_user");
+    setUserFullName(res.data.name);
   };
 
   return (

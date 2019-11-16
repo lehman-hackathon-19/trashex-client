4a689a704bd53c689f2b6e9f9395c371d195c104
Aswin Nair <aswintorch@gmail.com>
2019-11-16 05:53:47 -0500

    Added more styling

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NavbarLoggedIn from "../components/NavbarLoggedIn";
+import "./Dashboard.css";
 
 const Dashboard = () => {
- 
+  const [points, setPoints] = useState(0);
 
   return (
     <div className="Dashboard">
       <NavbarLoggedIn />
-      <h1>Home</h1>
+      <div className="container text-center pt-3">
+        <div className="circle small mx-auto">
+          <h1>{points}</h1>
+          <p>POINTS</p>
+        </div>
+      </div>
     </div>
   );
 };
